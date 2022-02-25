2ceb1236e4f7caf6f63714d5bfca61e20cbc6fea
tacticalCoder <kjones.roguesoftware@gmail.com>
2022-02-25 12:03:22 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,16 +6,11 @@ setwd("~/Desktop/NewR/Data/project-1/")
 power_data_table <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 
 # print(power_data_table)
-
-# Prevents histogram from printing in scientific notation
 power_data_table[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-
 # Change Date Column to Date Type
 power_data_table[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
-
 # Filter Dates for 2007-02-01 and 2007-02-02
 power_data_table <- power_data_table[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
-
 # create png
 png("plot1.png", width=480, height=480)
 
