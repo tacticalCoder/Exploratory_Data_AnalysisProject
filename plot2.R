7ebb460d8b59d4b3dd3ec224147a2bed47b03032
tacticalCoder <kjones.roguesoftware@gmail.com>
2022-02-25 12:04:05 -0800

    Update to code notes

--- plot2.R
+++ plot2.R
@@ -4,13 +4,9 @@ setwd("~/Desktop/NewR/Data/project-1/")
 
 # Read in data from data set, remove N/A's
 power_data_table <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
-
-# Prevents Scientific Notation
 power_data_table[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-
 # Making a POSIXct date capable of being filtered and graphed by time of day
 power_data_table[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
-
 # Filter Dates for 2007-02-01 and 2007-02-02
 power_data_table <- power_data_table[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
